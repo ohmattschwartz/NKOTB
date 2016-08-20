ace170e214fa2b38132d0968fb1066297930565c
Darren Thomas Clark <darren.clark@live.com>
2016-08-20 14:45:29 -0400

    polisdhed up html/css. mtv logo hidden. need help.

---
+++ src/Hype.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class Hype extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src='images/NKOTBlogo_Red.png' alt=''/>
+        </div>
+        <div className="App-body">
+        //BRAXTON --- TARGET THIS DIV WHEN YOU FETCH THE CODE FROM NYT.....IT CAN LOOK TOTALLY UGLY. NO BIGGY. WE'LL MAKE IT SEXY
+        //BRAXTON --- TARGET THIS DIV WHEN YOU FETCH THE CODE FROM NYT.....IT CAN LOOK TOTALLY UGLY. NO BIGGY. WE'LL MAKE IT SEXY
+        //BRAXTON --- TARGET THIS DIV WHEN YOU FETCH THE CODE FROM NYT.....IT CAN LOOK TOTALLY UGLY. NO BIGGY. WE'LL MAKE IT SEXY
+        //BRAXTON --- TARGET THIS DIV WHEN YOU FETCH THE CODE FROM NYT.....IT CAN LOOK TOTALLY UGLY. NO BIGGY. WE'LL MAKE IT SEXY
+        <div className='BRAXdump'></div>
+        //BRAXTON --- USE ABOVE DIV
+        //BRAXTON --- USE ABOVE DIV
+        //BRAXTON --- USE ABOVE DIV
+        //BRAXTON --- USE ABOVE DIV
+      </div>
+        <div className="App-footer">
+         <ul>
+          <li>Copyright 1990</li>
+          <li>Interscope Records</li>
+          <li>
+            <img src='images/Interscope_logo.png' alt=''/>
+          </li>
+         </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hype
