1022517d14ae5f15df1b11fd7710be8d02aa5c23
Darren Thomas Clark <darren.clark@live.com>
2016-08-20 13:27:25 -0400

    Media page inc. New images and css inc.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Song from './Song';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Song />,
   document.getElementById('root')
 );
